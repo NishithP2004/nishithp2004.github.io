ee67bf26a01f0430201151a18168a1dad52af7b2
Nishith P <nishithp11@gmail.com>
2022-09-26 23:33:09 +0530

    New Website

---
+++ type.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function(event){
+    var dataText = [ "Coder", "Google Code-in 2019 Finalist", "Google Code to Learn 2019 🏆", "Google enthusiast", "Developer", "a eighteen y/o"];
+    function typeWriter(text, i, fnCallback) {
+      if (i < (text.length)) {
+       document.getElementById("bio").innerHTML = text.substring(0, i+1) +' <span id="blinking-cursor">|</span>';
+  
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 150);
+      } else if (typeof fnCallback == 'function') {
+        setTimeout(fnCallback, 700);
+      }
+    }
+
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 5000);
+       }
+      if (i < dataText[i].length) {
+       typeWriter(dataText[i], 0, function(){
+         StartTextAnimation(i + 1);
+       });
+      }
+    }
+    StartTextAnimation(0);
+  });
